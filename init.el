7b6d331036650435db45ee393a36bc1f27de2016
Nacho Martinez Garrido <ignacio.martinez@gameloft.com>
2024-10-09 11:41:06 +0200

    Updated c++ stuff

--- init.el
+++ init.el
@@ -9,7 +9,6 @@
                        gcs-done)))
 
 ;;; PACKAGE
-
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
@@ -18,35 +17,14 @@
 (defun nm/get-default-font()
     "Consolas-13")
 (add-to-list 'default-frame-alist `(font . ,(nm/get-default-font)))
+(set-face-attribute 'default nil :height 110)
+
+;;; THEMES
+(load-theme 'modus-vivendi t)
 
 ;;; Don't add custom setting here
 (setq custom-file "~/.emacs.d/emacs-custom.el")
 (load custom-file t)
-
-
-;;; coding 
-
-(require 'cc-mode)
-
-(setq auto-mode-alist
-      (append
-       '(("\\.cpp$"     . c++-mode)
-         ("\\.hpp$"     . c++-mode)
-         ("\\.c$"       . c++-mode)
-         ("\\.h$"       . c++-mode)
-	 ("\\.inl$"     . c++-mode)
-         ("\\.emacs$"   . emacs-lisp-mode)
-	 ("\\.claradb$" . js-mode)
-         ("\\.txt$"     . indented-text-mode)
-	 ("\\.ts$"      . typescript-mode)
-	 ("\\.rml$"     . poly-rml-mode)
-	 ("\\.rcss$"    . css-mode)
-	 ("\\.lua$"     . lua-mode))
-       auto-mode-alist))
-
-(add-to-list 'auto-mode-alist '(".*\\(prj\\|premake\\).*\\.lua$" . indented-text-mode))
-
-(ido-mode)
 
 ;;; Interface tweaks
 (menu-bar-mode 0)
@@ -54,6 +32,7 @@
 (scroll-bar-mode 0)
 (global-display-line-numbers-mode)
 (show-paren-mode 1)
+(tab-bar-mode -1)
 
 (defun nm/paste-from-clipboard()
   (interactive)
@@ -67,8 +46,6 @@
   (kill-ring-save (region-beginning) (region-end))
   (setq x-select-enable-clipboard nil))
 
-(load-theme 'tango-dark)
-
 (set-charset-priority 'unicode)
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
@@ -77,7 +54,29 @@
 '(keyboard-coding-system 'utf-8)
 
 (global-hl-line-mode t)
-(setq backup-directory-alist            '((".*" . "~/.emacs/.Trash")))
+(setq backup-directory-alist `(("." . "~/.emacs.d/Trash")))
+
+(setq auto-save-file-name-transforms `((".*" "~/.emacs.d/auto-save-list/" t)))
+
+(setq auto-save-list-file-prefix "~/.emacs.d/autosave/")
+(setq auto-save-file-name-transforms
+      '((".*" "~/.emacs.d/autosave/" t)))
+
+;; Set grep and find locations
+(when (eq system-type 'windows-nt)
+  (setq exec-path (add-to-list 'exec-path "C:\\\"Program Files\"\\Git\\usr\\bin"))
+  (setq find-program "C:\\\"Program Files\"\\Git\\usr\\bin\\find.exe")
+  (setq grep-program "C:\\\"Program Files\"\\Git\\usr\\bin\\grep.exe")
+  (defadvice grep-compute-defaults (around grep-compute-defaults-advice-null-device)
+    "Use cygwin's /dev/null as the null-device."
+    (let ((null-device "/dev/null"))
+    ad-do-it))
+    (ad-activate 'grep-compute-defaults))
+
+;; Switch to grep buffer afeter invoked
+(with-eval-after-load 'rgrep
+  (advice-add 'rgrep :after
+	      #'(lambda (_pattern _files _dir &optional _literal _confirm _flags) (pop-to-buffer (rg-buffer-name)))))
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
@@ -122,6 +121,11 @@
                (lambda ()
                  (evil-local-set-key ',state (kbd ,key)
                                      ',fn)))))
+
+;;; project.el
+(require 'project)
+(global-set-key (kbd "M-O") 'project-find-file)
+(global-set-key (kbd "M-s") 'rgrep)
 
 ;;; undo-tree
 (use-package undo-tree
@@ -229,6 +233,15 @@
    '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
    '(rainbow-delimiters-depth-8-face ((t (:foreground "siennal"))))))
 
+;;Eglot
+
+(use-package eglot
+  :ensure t
+  :config
+  ;; Configura eglot para usar clangd
+  (add-to-list 'eglot-server-programs '(c++-mode . ("clangd")))
+  :hook (c++-mode . eglot-ensure))
+
 ;;; COMPANY MODE
 
 (use-package company
@@ -236,10 +249,71 @@
   :hook ((emacs-lisp-mode . (lambda ()
                               (setq-local company-backends '(company-elisp))))
          (emacs-lisp-mode . company-mode))
+         (c++-mode . company-mode)
   :config
-  (setq company-idle-delay 0.1
-        company-minimum-prefix-length 2))
+  (setq company-idle-delay 0.0
+        company-minimum-prefix-length 2)
+   (setq company-backends '((company-capf)))) 
 
+;;; clang-format
+
+(use-package clang-format
+  :ensure t)
+
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c f") 'clang-format-region)
+            ;; (add-hook 'before-save-hook 'clang-format-buffer nil 'local)
+            ))
+
+(add-hook 'c-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c f") 'clang-format-region)
+            ;; (add-hook 'before-save-hook 'clang-format-buffer nil 'local)
+            ))
+(defun clang-format-get-options ()
+  "Parse the .clang-format file and return options as an alist."
+  (let ((options '()))
+    (with-temp-buffer
+      ;; Leer el archivo .clang-format
+      (insert-file-contents (expand-file-name ".clang-format" (project-root (project-current))))
+      (goto-char (point-min))
+      (while (re-search-forward "\\([a-zA-Z0-9_]+\\): \\([0-9]+\\|true\\|false\\|[^\n]+\\)" nil t)
+        (let* ((key (intern (match-string 1)))
+               (value (match-string 2)))
+          (cond
+           ((string-match-p "^[0-9]+$" value)
+            (setq value (string-to-number value)))
+           ((string= value "true") ;; Si es "true"
+            (setq value t))
+           ((string= value "false") ;; Si es "false"
+            (setq value nil)))
+          (setq options (cons (cons key value) options)))))
+    options))
+
+(defun set-clang-format-style-from-file ()
+  "Set indentation style based on the .clang-format file in the project's root directory."
+  (when (require 'project nil 'noerror) 
+    (let ((project-root (project-current)))
+      (when (and project-root
+                 (file-exists-p (expand-file-name ".clang-format" (project-root project-root))))
+        (setq clang-format-style "file")
+        (message "Found .clang-format in project root: %s" (project-root project-root))
+        (let ((clang-format-options (clang-format-get-options)))
+          (let ((indent-width (cdr (assq 'IndentWidth clang-format-options)))
+                (use-tab (cdr (assq 'UseTab clang-format-options)))
+                (tab-width (cdr (assq 'TabWidth clang-format-options))))
+            ;; Asegurarse de que los valores sean enteros
+            (setq c-basic-offset (if (integerp indent-width) indent-width 4)) ;; Valor por defecto
+            (setq indent-tabs-mode (if use-tab t nil)) ;; Usar tabulaciones o espacios
+            (setq tab-width (if (integerp tab-width) tab-width 4)) ;; Valor por defecto
+            (setq c++-tab-always-indent t)  ;; Indentar siempre con tabulaciones
+            (message "Applied .clang-format settings: IndentWidth=%d, UseTab=%s, TabWidth=%d"
+                     c-basic-offset
+                     indent-tabs-mode
+                     tab-width)))))))
+(add-hook 'c++-mode-hook 'set-clang-format-style-from-file)
+(add-hook 'c-mode-hook 'set-clang-format-style-from-file)
 ;;; WINUM
 
 (use-package winum
@@ -264,4 +338,41 @@
 (use-package flycheck
   :ensure t)
 
+;;; coding 
 
+(require 'cc-mode)
+(use-package lua-mode
+  :ensure t
+  :config
+    (setq lua-indent-level 4)
+    (setq lua-default-application "lua"))
+
+(setq auto-mode-alist
+      (append
+       '(("\\.cpp$"     . c++-mode)
+         ("\\.hpp$"     . c++-mode)
+         ("\\.c$"       . c++-mode)
+         ("\\.h$"       . c++-mode)
+	 ("\\.inl$"     . c++-mode)
+         ("\\.emacs$"   . emacs-lisp-mode)
+	 ("\\.claradb$" . js-mode)
+         ("\\.txt$"     . indented-text-mode)
+	 ("\\.ts$"      . typescript-mode)
+	 ("\\.rml$"     . poly-rml-mode)
+	 ("\\.rcss$"    . css-mode)
+	 ("\\.lua$"     . lua-mode))
+       auto-mode-alist))
+
+(add-to-list 'auto-mode-alist '(".*\\(prj\\|premake\\).*\\.lua$" . indented-text-mode))
+
+(ido-mode)
+
+;;; Keybindings
+
+(global-set-key (kbd "C-<tab>") 'my-cycle-project-buffers)
+
+(global-set-key (kbd "C-<tab>") 'bs-cycle-next)
+(global-set-key (kbd "C-S-<tab>") 'bs-cycle-previous)
+(global-set-key (kbd "C-l") 'ibuffer)
+
+(global-set-key (kbd "C-q") 'kill-this-buffer)
